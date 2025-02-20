424823cdac344eb3aea64937f388e15fc41bca22
Sam Persson <sam.persson@visma.com>
2025-02-20 14:20:22 +0100

    v12.0.7

--- scripts/set-version.ts
+++ scripts/set-version.ts
@@ -1,4 +1,5 @@
-import { readJson, writeJson } from 'fs-extra';
+import pkg from 'fs-extra';
+const { readJson, writeJson } = pkg;
 import { sync as glob } from 'glob';
 
 const packagesGlob = './src/**/package.json';
@@ -7,20 +8,19 @@ const docsVersion = './apps/ngx-bootstrap-docs/src/assets/json/current-version.j
 const schematicDependencyVersion = './src/schematics/src/utils/current_dependency_versions.json';
 
 (async () => {
-  const version = await readJson(mainPackage).then(json => json.version);
-  const packages = [docsVersion, schematicDependencyVersion, ...glob(packagesGlob)]
-    .map(async packagePath => {
-      const packageJson = await readJson(packagePath);
-      if (packageJson.version) {
-        packageJson.version = version;
-      }
+  const version = await readJson(mainPackage).then((json) => json.version);
+  const packages = [docsVersion, schematicDependencyVersion, ...glob(packagesGlob)].map(async (packagePath) => {
+    const packageJson = await readJson(packagePath);
+    if (packageJson.version) {
+      packageJson.version = version;
+    }
 
-      if (packageJson.NGX_BOOTSTRAP_VERSION) {
-        packageJson.NGX_BOOTSTRAP_VERSION = version;
-      }
+    if (packageJson.NGX_BOOTSTRAP_VERSION) {
+      packageJson.NGX_BOOTSTRAP_VERSION = version;
+    }
 
-      await writeJson(packagePath, packageJson, { spaces: 2 });
-    });
+    await writeJson(packagePath, packageJson, { spaces: 2 });
+  });
 
   await Promise.all(packages);
 })();
